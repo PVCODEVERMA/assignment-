cb869dedff34e235c1ae1f1f2f23632ba862681f
pankaj verma <117702000+verma421@users.noreply.github.com>
2023-01-30 11:38:39 +0530

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+   float num1, num2;
+   cout <<"Enter the first number ";
+   cin >> num1;
+   cout << "Enter the second number ";
+   cin >> num2;
+   cout <<"sum = " << num1 + num2<<endl;
+   cout <<"Difference = " << num1 + num2<<endl;
+   cout <<"product =" << num1 * num2<<endl;
+   cout << "Division = " << num1 / num2<<endl;
+   return 0;
+}
